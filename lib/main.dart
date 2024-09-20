ef3211458245aeccc3561fd565bcce8cfa0a6567
Ibrahim Khan <khan.ibrahim1336@gmail.com>
2024-09-19 17:48:03 -0700

    changing

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {jjkjkj
+  Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => MyAnimation()),
